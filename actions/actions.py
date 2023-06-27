92bf79cdd1bb44e6c3ffb9bd88a11a028a9460c9
Cristian <42221682+Becessj@users.noreply.github.com>
2023-06-27 15:46:58 -0500

    Update actions.py
    final update

--- actions/actions.py
+++ actions/actions.py
@@ -198,45 +198,45 @@ class ActionConsultarRequisitosTramite(Action):
         return []
 
 
-class ActionSaveConversation(Action):
+# class ActionSaveConversation(Action):
 
-    def name(self) -> Text:
-        return "action_say_name"
+#     def name(self) -> Text:
+#         return "action_say_name"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        conversation=tracker.events
-        print(conversation)
-        import os
-        if not os.path.isfile('chats.csv'):
-            with open('chats.csv','w') as file:
-                file.write("intent,user_input,entity_name,entity_value,action,bot_reply\n")
-        chat_data=''
-        for i in conversation:
-            if i['event'] == 'user':
-                chat_data+=i['parse_data']['intent']['name']+','+i['text']+','
-                print('user: {}'.format(i['text']))
-                if len(i['parse_data']['entities']) > 0:
-                    chat_data+=i['parse_data']['entities'][0]['entity']+','+i['parse_data']['entities'][0]['value']+','
-                    print('extra data:', i['parse_data']['entities'][0]['entity'], '=',
-                          i['parse_data']['entities'][0]['value'])
-                else:
-                    chat_data+=",,"
-            elif i['event'] == 'bot':
-                print('Bot: {}'.format(i['text']))
-                try:
-                    chat_data+=i['metadata']['utter_action']+','+i['text']+'\n'
-                except KeyError:
-                    pass
-        else:
-            with open('chats.csv','a') as file:
-                file.write(chat_data)
+#         conversation=tracker.events
+#         print(conversation)
+#         import os
+#         if not os.path.isfile('chats.csv'):
+#             with open('chats.csv','w') as file:
+#                 file.write("intent,user_input,entity_name,entity_value,action,bot_reply\n")
+#         chat_data=''
+#         for i in conversation:
+#             if i['event'] == 'user':
+#                 chat_data+=i['parse_data']['intent']['name']+','+i['text']+','
+#                 print('user: {}'.format(i['text']))
+#                 if len(i['parse_data']['entities']) > 0:
+#                     chat_data+=i['parse_data']['entities'][0]['entity']+','+i['parse_data']['entities'][0]['value']+','
+#                     print('extra data:', i['parse_data']['entities'][0]['entity'], '=',
+#                           i['parse_data']['entities'][0]['value'])
+#                 else:
+#                     chat_data+=",,"
+#             elif i['event'] == 'bot':
+#                 print('Bot: {}'.format(i['text']))
+#                 try:
+#                     chat_data+=i['metadata']['utter_action']+','+i['text']+'\n'
+#                 except KeyError:
+#                     pass
+#         else:
+#             with open('chats.csv','a') as file:
+#                 file.write(chat_data)
 
-        dispatcher.utter_message(text="All Chats saved.")
+#         dispatcher.utter_message(text="All Chats saved.")
 
-        return []
+#         return []
         
 class ActionRestart(Action):
     """
