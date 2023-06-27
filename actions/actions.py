599201a98f8e5a344105adffd2cc8a5e439d94f8
Cristian <42221682+Becessj@users.noreply.github.com>
2023-06-26 21:44:12 -0500

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -11,56 +11,52 @@ from unidecode import unidecode
 
 PLADDESAPI_URL = "https://tramite.unsaac.edu.pe/tramite/buscar_expediente_x_codigo"
 
-""" class DefaultFallback(Action):
+class DefaultFallback(Action):
     def name(self):
         return "my_fallback_action"
 
     def run(self, dispatcher, tracker, domain):
         response = "Mmmm, no estoy seguro de lo que quieres decirme..." + "\n" + "Me puedes preguntar sobre  \n ● Mis trámites \n  ● Información de mi trámite \n ● Requisitos de este trámite"
-        dispatcher.utter_message(text=response) """
+        dispatcher.utter_message(text=response)
 
-class ActionCustomFallback(Action):
+# class ActionCustomFallback(Action):
 
-    def name(self) -> Text:
-        return "action_custom_fallback"
+#     def name(self) -> Text:
+#         return "action_custom_fallback"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        intents = [i for i in tracker.current_state()['latest_message']['intent_ranking'] if i['name'] != 'nlu_fallback']
+#         intents = [i for i in tracker.current_state()['latest_message']['intent_ranking'] if i['name'] != 'nlu_fallback']
         
-        allowed_intents = ["consultar_tramites", "consultar_descripcion", "consultar_monto"]
-        message = {
-            "consultar_tramites": "Que tramites tengo?",
-            "consultar_descripcion": " Qué es el Pase para Escolares?",
-            "consultar_monto": "Cuánto es el costo del tramite Traslado Externo?"
-        }
-        buttons = [{'payload': i['name'], 'title': message[i['name']]} for i in intents[:3] if i['name'] in allowed_intents]
-        dispatcher.utter_message(
-            text="No estaba 100% claro lo que querías decir. ¿Podría especificar/reformular?",
-            buttons=buttons
-        )
-        return []
+#         allowed_intents = ["consultar_tramites", "consultar_descripcion", "consultar_monto"]
+#         message = {
+#             "consultar_tramites": "Que tramites tengo?",
+#             "consultar_descripcion": " Qué es el Pase para Escolares?",
+#             "consultar_monto": "Cuánto es el costo del tramite Traslado Externo?"
+#         }
+#         buttons = [{'payload': i['name'], 'title': message[i['name']]} for i in intents[:3] if i['name'] in allowed_intents]
+#         dispatcher.utter_message(
+#             text="No estaba 100% claro lo que querías decir. ¿Podría especificar/reformular?",
+#             buttons=buttons
+#         )
+#         return []
 
     
-class ActionDepedida(Action):
-    """
-    ActionHelloWorld Recoge el nombre de usuario y ademas da un mensaje de bienvenida que se envia
-    al modulo conversacional del Bot 
-    """
-    def name(self) -> Text:
-        return "action_despedida"
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        name = tracker.get_slot('name')
-
-        dispatcher.utter_message(template="utter_goodbye", buttons = [
-            {"payload": "/bueno", "title": "Bueno"},
-            {"payload": "/malo", "title": "Malo"},
-        ])
-        return []
+# class ActionHelloWorld(Action):
+#     """
+#     ActionHelloWorld Recoge el nombre de usuario y ademas da un mensaje de bienvenida que se envia
+#     al modulo conversacional del Bot 
+#     """
+#     def name(self) -> Text:
+#         return "action_welcome"
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         name = tracker.get_slot('name')
+#         dispatcher.utter_message(text=f"Bienvenid@ {name}, si deseas puedo buscar requisitos, descripcion de tu tramite, o ver el estado de tu tramite ¿Qué te gustaría hacer?")
+#         return []
 
 class ActionSayName(Action):
     def name(self) -> Text:
@@ -88,12 +84,12 @@ class ActionConsultarTramites(Action):
             {"payload":"/"},
             {}
         ]
-        año = tracker.get_slot('año')
+        anio = tracker.get_slot('anio')
         codigo = tracker.get_slot('codigo')
 
-        print("Los datos que han llegado son:  " + año + " " +codigo)
+        print("Los datos que han llegado son:  " + anio + " " +codigo)
 
-        the_data = {"anio": año, "codigo": codigo}
+        the_data = {"anio": anio, "codigo": codigo}
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         # Execute the post
         response = requests.post(PLADDESAPI_URL, data=the_data, headers=headers).text
@@ -107,10 +103,10 @@ class ActionConsultarTramites(Action):
             dispatcher.utter_message(template="utter_tramite_no_encontrado")
             return [AllSlotsReset()]
         else:
-            dispatcher.utter_message(text=f"Tienes los siguientes tramites durante el año {año} \n \n")
+            dispatcher.utter_message(text=f"Tienes los siguientes tramites durante el año {anio} \n \n")
             for k in range(len(tramites)):
-                dispatcher.utter_message(text=f"{k+1}.-{str(tramites[k])}")
-        result = [SlotSet("año", año),SlotSet("codigo", codigo)]
+                dispatcher.utter_message(text=f"{str(tramites[k])}")
+        result = [SlotSet("anio", anio),SlotSet("codigo", codigo)]
         return result
 
 class ActionConsultarDescripcionTramite(Action):
@@ -137,12 +133,9 @@ class ActionConsultarDescripcionTramite(Action):
 
         usuario_nombre_tramite = tracker.get_slot("nombre_tramite")
         Resultado = self.Buscar_Info(usuario_nombre_tramite)
-        if(Resultado.size>0):
-            Nombre = Resultado["nombre"][0]
-            Descripcion = Resultado["descripcion"][0]
-            dispatcher.utter_message(text=f"El trámite {Nombre} \n\n es un {Descripcion}")
-        else:
-            dispatcher.utter_message(template="utter_fallback")
+        Nombre = Resultado["nombre"][0]
+        Descripcion = Resultado["descripcion"][0]
+        dispatcher.utter_message(text=f"El trámite {Nombre} es un {Descripcion}")
         return []
 
 class ActionConsultarMontoTramite(Action):
@@ -169,12 +162,9 @@ class ActionConsultarMontoTramite(Action):
 
         usuario_nombre_tramite = tracker.get_slot("nombre_tramite")
         Resultado = self.Buscar_Info(usuario_nombre_tramite)
-        if(Resultado.size>0):
-            Nombre = Resultado["nombre"][0]
-            Monto = Resultado["monto"][0]
-            dispatcher.utter_message(text=f"El costo del trámite {Nombre} \n\n es de S/.{Monto}")
-        else:
-            dispatcher.utter_message(template="utter_fallback")
+        Nombre = Resultado["nombre"][0]
+        Monto = Resultado["monto"][0]
+        dispatcher.utter_message(text=f"El costo del trámite {Nombre} es de S/.{Monto}")
         return []
 
 class ActionConsultarRequisitosTramite(Action):
@@ -202,20 +192,16 @@ class ActionConsultarRequisitosTramite(Action):
 
         usuario_nombre_tramite = tracker.get_slot("nombre_tramite")
         Requisitos = self.Buscar_Info(usuario_nombre_tramite)
-        if(Requisitos.size>0):
-            Nombre = Requisitos["nombre"][0]
-            Req = Requisitos["requisitos"][0]
-            dispatcher.utter_message(text=f"Los requisitos del trámite {Nombre} son:\n\n {Req}")
-        else:
-            dispatcher.utter_message(template="utter_fallback")
-            
+        Nombre = Requisitos["nombre"][0]
+        Requisitos = Requisitos["requisitos"][0]
+        dispatcher.utter_message(text=f"Los requisitos del trámite {Nombre} son:\n{Requisitos}")
         return []
 
 
 class ActionSaveConversation(Action):
 
     def name(self) -> Text:
-        return "action_save_conversation"
+        return "action_say_name"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -252,6 +238,16 @@ class ActionSaveConversation(Action):
 
         return []
         
-
+class ActionRestart(Action):
+    """
+    ActionHelloWorld Recoge el nombre de usuario y ademas da un mensaje de bienvenida que se envia
+    al modulo conversacional del Bot 
+    """
+    def name(self) -> Text:
+        return "action_restart"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         return [Restarted()]
 
 
